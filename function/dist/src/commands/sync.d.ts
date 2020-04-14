97aefd23c897ccf22320c7ddfa64ccb231bfb9d3
Jamie Spittal <lastmanout@gmail.com>
2020-04-14 14:20:43 -0700

    pushing 0.6.3 dist

---
+++ function/dist/src/commands/sync.d.ts
@@ -0,0 +1,2 @@
+import { SyncCommandOptions } from '../types';
+export declare function sync({ vueFiles, accessToken, projectID, tags, makeTranslation, skipReport, dryRun, outputDir, }: SyncCommandOptions): Promise<void>;
